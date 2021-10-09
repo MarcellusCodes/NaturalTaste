b2e753c9abd5b965acb8a2555386341dbdbf5818
MarcellusCodes <MarcellusCodes@gmail.com>
2021-10-09 16:01:18 +0200

    Configured Tailwind Theme to extend and not overwrite Colors, added Wavy Boxshadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    colors: {
-      primary: "#FF6E00",
-      secondary: "#96CFCE",
-      white: "#FFFFFF",
+    extend: {
+      colors: {
+        primary: "#FF6E00",
+        secondary: "#96CFCE",
+        white: "#FFFFFF",
+      },
+      boxShadow: {
+        dreamy:
+          "0 1px 2px rgba(0,0,0,0.07), 0 2px 4px rgba(0,0,0,0.07), 0 4px 8px rgba(0,0,0,0.07), 0 8px 16px rgba(0,0,0,0.07),0 16px 32px rgba(0,0,0,0.07), 0 32px 64px rgba(0,0,0,0.07)",
+      },
     },
   },
   variants: {
